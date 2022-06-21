91efc9de071f44214791abc17316a10d89b90334
[Norah Taylor] <emt368@gmail.com>
2022-06-21 13:38:17 -0400

    made application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json
   end
 
+  get "/salons" do
+    salons = Salon.all
+    salons.to_json
+  end
+
+  post "/appointments" do 
+    new_appointment = Appointment.create(
+      username: params[:username],
+      date: params[:date],
+      appointment_type: params[:appointment_type]
+    )
+    new_appointment.to_json
+  end 
+
+
+  delete '/appointments/:id' do 
+    delete = Appointment.find(params[:id])
+    delete.destroy
+    delete.to_json
+  end 
+
+  patch '/appointments/:id' do 
+    appointment = Appointment.find(params[:id])
+    appointment.update(
+      username: params[:username],
+      date: params[:date],
+      appointment_type: params[:appointment_type]
+    )
+    appointment.to_json
 end
